37673eaf95782197887af3f2ee5b58395a78911a
istott <istott@technologypartners.net>
2022-02-24 11:44:27 -0600

    first commit

--- packages/react-search-ui/src/__tests__/helpers.test.js
+++
@@ -1,9 +0,0 @@
-import { accentFold } from "../helpers";
-
-describe("accentFold", () => {
-  it("should remove accents marks from string", () => {
-    expect(accentFold("ǟc̈ŗÄh")).toBe("acrAh");
-    expect(accentFold("abc")).toBe("abc");
-    expect(accentFold("ABC")).toBe("ABC");
-  });
-});
