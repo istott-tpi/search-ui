7822c563e8834d17bf54049038b666bc968497bf
istott <istott@technologypartners.net>
2022-03-07 08:05:41 -0600

    style changes

--- app/src/components/searchUI.jsx
+++ app/src/components/searchUI.jsx
@@ -1,41 +1,13 @@
-import React, {useState} from "react";
+import React from "react";
 
-import Paper from '@mui/material/Paper';
-import {InputBase, Autocomplete} from '@mui/material';
-import IconButton from '@mui/material/IconButton';
-import Button from '@mui/material/Button';
+import {Paper, InputBase, Box, IconButton, Button} from '@mui/material';
 import { IoFilterSharp } from "react-icons/io5";
 import SearchIcon from '@mui/icons-material/Search';
-import { MdLocationPin } from "react-icons/md";
-// import { useAppSelector } from '../store';
-// import { ILocation } from '../zuma/location/interfaces';
 
 import AppSearchAPIConnector from "@elastic/search-ui-app-search-connector";
-
-import {
-  ErrorBoundary,
-  Facet,
-  SearchProvider,
-  SearchBox,
-  Results,
-  Result,
-  PagingInfo,
-  ResultsPerPage,
-  Paging,
-  Sorting,
-  WithSearch
-} from "@elastic/react-search-ui";
-import { Layout } from "@elastic/react-search-ui-views";
+import {SearchProvider, WithSearch} from "@elastic/react-search-ui";
 import "@elastic/react-search-ui-views/lib/styles/styles.css";
-
-import {
-  buildAutocompleteQueryConfig,
-  buildFacetConfigFromConfig,
-  buildSearchOptionsFromConfig,
-  buildSortOptionsFromConfig,
-  getConfig,
-  getFacetFields
-} from "./config-helper";
+import {buildAutocompleteQueryConfig, buildFacetConfigFromConfig, buildSearchOptionsFromConfig, getConfig} from "./config-helper";
 
 const { hostIdentifier, searchKey, endpointBase, engineName } = getConfig();
 const connector = new AppSearchAPIConnector({
@@ -55,7 +27,6 @@ const config = {
 };
 
 export default function SearchUI() {
-  // const [searchTerm, setSearchTerm] = useState('');
 
   return (
     <>
@@ -65,125 +36,44 @@ export default function SearchUI() {
         {({ wasSearched, searchTerm, setSearchTerm, results }) => {
           console.log(results)
           return (
-            <div className="App">
-              <ErrorBoundary>
-                <Layout
-                  header={  
-                    <>    
-                    <Paper
-                        className="main-search"
-                        component="form"
-                        sx={{ p: '2px 4px', display: 'flex', flexDirection: 'column', alignItems: 'center', maxHeight: 400,  width: 400, overflow: 'auto' }}
-                    >
-                      <div>
-                        <IconButton sx={{ p: '10px' }} aria-label="menu">
-                            <SearchIcon />
-                        </IconButton>
-                        <InputBase
-                            sx={{ ml: 1, flex: 1 }}
-                            placeholder="Search"
-                            inputProps={{ 'aria-label': 'search google maps' }}
-                            value={searchTerm}
-                            onChange={e => setSearchTerm(e.target.value)}
-                        />
-                        <IconButton type="submit" sx={{ p: '10px' }} aria-label="search">
-                            <IoFilterSharp />
-                        </IconButton>
-                        </div>
-                        <div>
-                          {wasSearched && results.map(result => (
-                              <div key={result.id.raw}>
-                                <span>Client Name: {result?.name?.raw}</span>
-                                
-                              </div>
-        
-                          ))}
-                        </div>
-                    </Paper> 
-
-                    </>
-                  }
-                  // sideContent={
-                  //   <div>
-                  //     {wasSearched && (
-                  //       <Sorting
-                  //         label={"Sort by"}
-                  //         sortOptions={buildSortOptionsFromConfig()}
-                  //       />
-                  //     )}
-                  //     {getFacetFields().map(field => (
-                  //       <Facet key={field} field={field} label={field} />
-                  //     ))}
-                  //   </div>
-                  // }
-                  // bodyHeader={
-                  //   <React.Fragment>
-                  //     {wasSearched && <PagingInfo />}
-                  //     {wasSearched && <ResultsPerPage />}
-                  //   </React.Fragment>
-                  // }
-                  // bodyContent={
-                  //   <Results
-                  //     titleField={getConfig().titleField}
-                  //     urlField={getConfig().urlField}
-                  //     thumbnailField={getConfig().thumbnailField}
-                  //     shouldTrackClickThrough={true}
-                  //     onChange={() => console.log(getFacetFields())}
-
-                  //   />
-
-                  // }
-                  // bodyFooter={<Paging />}
+            <Paper sx={{height: '100vh', width: '100vw', padding: '20px 0 0 20px', backgroundColor: 'blue'}} >
+            <Paper
+                className="main-search"
+                component="form"
+                sx={{ p: '2px 4px', display: 'flex', flexDirection: 'column', alignItems: 'center', maxHeight: 400,  width: 400, overflow: 'auto' }}
+            >
+              <Box sx={{width: '100%', display: 'flex', justifyContent: 'space-between' }} >
+                <IconButton sx={{ p: '10px' }} aria-label="menu">
+                    <SearchIcon />
+                </IconButton>
+                <InputBase
+                    sx={{ ml: 1, flex: 1 }}
+                    placeholder="Search"
+                    inputProps={{ 'aria-label': 'search google maps' }}
+                    value={searchTerm}
+                    onChange={e => setSearchTerm(e.target.value)}
                 />
-              </ErrorBoundary>
-            </div>
+                <IconButton type="submit" sx={{ p: '10px' }} aria-label="search">
+                    <IoFilterSharp />
+                </IconButton>
+              </Box>
+              {!!searchTerm &&
+                <Box sx={{overflow: 'auto', width: '100%'}} >
+                  {wasSearched && results.map(result => (
+                      <div key={result.id.raw}>
+                        <Button>Client Name: {result?.name?.raw}</Button>
+                        
+                      </div>
+
+                  ))}
+                </Box>
+              }
+            </Paper> 
+            </Paper>
           );
         }}
       </WithSearch>
     </SearchProvider>
-    {/* <Paper
-        className="main-search"
-        component="form"
-        sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', height: '2rem', width: 400 }}
-    >
-        <IconButton sx={{ p: '10px' }} aria-label="menu">
-            <SearchIcon />
-        </IconButton>
-        <InputBase
-            sx={{ ml: 1, flex: 1 }}
-            placeholder="Search"
-            inputProps={{ 'aria-label': 'search google maps' }}
-            value={searchTerm}
-            onChange={e => setSearchTerm(e.target.value)}
-        />
-        <IconButton type="submit" sx={{ p: '10px' }} aria-label="search">
-            <IoFilterSharp />
-        </IconButton>
-    </Paper> */}
-    {/* <SearchProvider config={config}>
-      <WithSearch
-        mapContextToProps={({ searchTerm, setSearchTerm, results }) => ({
-          searchTerm,
-          setSearchTerm,
-          results
-        })}
-      >
-        {({ searchTerm, setSearchTerm, results }) => {
-          return (
-            <div>
-              <input
-                value={searchTerm}
-                onChange={e => setSearchTerm(e.target.value)}
-              />
-              {results.map(r => (
-                <div key={r.id.raw}>{r.title.raw}</div>
-              ))}
-            </div>
-          );
-        }}
-      </WithSearch>
-    </SearchProvider> */}
-    
     </>
   );
 }
